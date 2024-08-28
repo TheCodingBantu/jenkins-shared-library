88ee18d19d065a85ecd0706f13a92c9f16ea5fad
Your Name <you@example.com>
2024-08-28 20:25:00 +0300

    added parameters to git checkout

--- vars/checkoutGitRepo.groovy
+++ vars/checkoutGitRepo.groovy
@@ -2,6 +2,6 @@
 
 import com.example.Docker
 
-def call(String sl_epoUrl, String sl_branchName, String sl_credentialsId) {
-    return new Docker(this).checkoutGitRepo(String sl_repoUrl, String sl_branchName, String sl_credentialsId)
+def call(String repo_url, String branchName, String credentialsId) {
+    return new Docker(this).checkoutGitRepo(repoUrl, branchName, credentialsId)
 }
