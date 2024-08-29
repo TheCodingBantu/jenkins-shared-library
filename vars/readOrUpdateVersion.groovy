536ac7b61f55657ea665fa3ab47592cd5f5d7a7b
pngugi <pngugi@mail.com>
2024-08-29 12:50:18 +0300

    remove groovy shebang

--- vars/readOrUpdateVersion.groovy
+++ vars/readOrUpdateVersion.groovy
@@ -1,7 +1,3 @@
-// String action = 
-
-#!/usr/bin/env groovy
-
 import com.example.Docker
 
 def call(String action, String gitRepoUrl,String gitCredsId, String branchName, String versionFile , String defaultVersion ) {
