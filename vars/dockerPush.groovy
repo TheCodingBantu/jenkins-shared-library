8ebbf69e37c5ffe236a591dff65f35e75e33a1cc
pngugi <pngugi@mail.com>
2024-09-02 12:14:35 +0300

    debug

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,7 +1,7 @@
 
 import com.example.Docker
 
-def call(String registryUrl, String registryCreds, String releaseTag) {
+def call(String imageName, String fqdn, String registryUrl, String registryCreds, String releaseTag) {
     // REGISTRY_URL,REGISTRY_CREDS,env.IMAGEVERSION 
-    return new Docker(this).dockerPush(registryUrl, registryCreds, releaseTag)
+    return new Docker(this).dockerPush(imageName,fqdn, registryUrl, registryCreds, releaseTag)
 }
