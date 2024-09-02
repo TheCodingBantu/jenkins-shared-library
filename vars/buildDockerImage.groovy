5d88e312980ff127d251a3cd2c191621ee190f96
pngugi <pngugi@mail.com>
2024-09-02 08:39:58 +0300

    debug

--- vars/buildDockerImage.groovy
+++ vars/buildDockerImage.groovy
@@ -1,5 +1,5 @@
 import com.example.Docker
 
-def call(String imageName) {
-    return new Docker(this).buildDockerImage(imageName)
+def call(String imageName, String dockerfilePath, String buildArgs) {
+    return new Docker(this).buildDockerImage(imageName, dockerfilePath, buildArgs)
 }
