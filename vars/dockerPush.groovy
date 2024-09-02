143b240a00923ca256a0b498c2b3c12ff416b2ab
pngugi <pngugi@mail.com>
2024-09-02 10:28:15 +0300

    passing parameters to docker push

--- vars/dockerPush.groovy
+++ vars/dockerPush.groovy
@@ -1,6 +1,7 @@
 
 import com.example.Docker
 
-def call(String imageName) {
-    return new Docker(this).dockerPush(imageName)
+def call(String registryUrl, String registryCreds, String releaseTag) {
+    // REGISTRY_URL,REGISTRY_CREDS,env.IMAGEVERSION 
+    return new Docker(this).dockerPush(registryUrl, registryCreds, releaseTag)
 }
