b32aebe18d6ebe3295555b1ce1ddaa80ed64d722
pngugi <pngugi@mail.com>
2024-09-04 11:43:08 +0300

    reconfigure read/update actions

--- vars/readVersion.groovy
+++ vars/readVersion.groovy
@@ -1,5 +1,5 @@
 import com.example.Docker
 
 def call(String releaseType, String gitRepoUrl,String gitCredsId, String branchName, String versionFile , String defaultVersion ) {
-    return new Docker(this).readVersion(action, releaseType, gitRepoUrl,gitCredsId, branchName, versionFile , defaultVersion )
+    return new Docker(this).readVersion(releaseType, gitRepoUrl,gitCredsId, branchName, versionFile , defaultVersion )
 }
