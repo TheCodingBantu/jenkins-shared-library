39f356c8e378a9170eb7bcb73bd4b67d7a485303
pngugi <pngugi@mail.com>
2024-09-09 12:35:05 +0300

    added helm helper methods

--- src/com/example/Helm.groovy
+++ src/com/example/Helm.groovy
@@ -10,7 +10,9 @@ class Helm implements Serializable {
         try {
             //we can use docker registry
               script.docker.withRegistry(registryUrl, registryCreds) {
-               script.sh "helm push ${chartName} oci://${registryUrl}/${helmRepo}"
+                def strippedUrl = registryUrl.replaceAll(/^https?:\/\//, '')
+                script.echo "${strippedUrl}"
+               script.sh "helm push ${chartName} oci://${strippedUrl}/${helmRepo}"
             }
 
             } catch (Exception e) {
