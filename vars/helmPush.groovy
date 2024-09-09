4fe2277d105b71777f3e27494af69eb6260159ea
pngugi <pngugi@mail.com>
2024-09-09 12:18:03 +0300

    added helm helper methods

--- vars/helmPush.groovy
+++ vars/helmPush.groovy
@@ -2,5 +2,5 @@
 import com.example.Helm
 
 def call(String chartName, String registryUrl, String registryCreds, String helmRepo) {
-    return new Docker(this).helmPush(chartName,registryUrl, registryCreds, helmRepo)
+    return new Helm(this).helmPush(chartName,registryUrl, registryCreds, helmRepo)
 }
